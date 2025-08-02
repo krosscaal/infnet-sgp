bbc2db36e554139a9caaf0ff321c171d8ea8b16a
Krossby <krosscaal@gmail.com>
2025-08-02 15:11:26 -0300

    feature: Inicio Projeto

---
+++ src/main/java/br/edu/infnet/krossbyapi/KrossbyapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.krossbyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrossbyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrossbyapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/krossbyapi/KrossbyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.krossbyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrossbyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
