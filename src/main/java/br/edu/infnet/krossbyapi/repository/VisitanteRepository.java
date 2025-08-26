be740db3ba77266b037510c541130061c4e66f0e
Krossby <krosscaal@gmail.com>
2025-08-26 19:47:21 -0300

    remove: removendo imports não usados em VisitanteRepository

--- src/main/java/br/edu/infnet/krossbyapi/repository/VisitanteRepository.java
+++ src/main/java/br/edu/infnet/krossbyapi/repository/VisitanteRepository.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface VisitanteRepository extends JpaRepository<Visitante, Long> {
